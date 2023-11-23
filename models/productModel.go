667ced87c8cd36a969d26d297f9036f3699d06ad
Mangalam Kumar <mangalamkumar2002@gmail.com>
2023-11-24 00:00:36 +0530

    done with migrations

--- models/productModel.go
+++ models/productModel.go
@@ -3,17 +3,28 @@ package models
 import (
 	"time"
 
-	"gorm.io/gorm"
+	"github.com/lib/pq"
 )
 
+// type Product struct {
+// 	gorm.Model
+// 	ProductID               int
+// 	ProductName             string
+// 	ProductDescription      string
+// 	ProductImages           pq.StringArray
+// 	ProductPrice            float64
+// 	CompressedProductImages pq.StringArray
+// 	CreatedAt               time.Time
+// 	UpdatedAt               time.Time
+// }
+
 type Product struct {
-	gorm.Model
-	ProductID               int
-	ProductName             string
-	ProductDescription      string
-	ProductImages           []string
-	ProductPrice            float64
-	CompressedProductImages []string
-	CreatedAt               time.Time
-	UpdatedAt               time.Time
+	ProductID               int            `gorm:"primary_key" json:"product_id"`
+	ProductName             string         `json:"product_name"`
+	ProductDescription      string         `json:"product_description"`
+	ProductImages           pq.StringArray `json:"product_images" gorm:"type:text[]"`
+	ProductPrice            float64        `json:"product_price"`
+	CompressedProductImages pq.StringArray `json:"compressed_product_images" gorm:"type:text[]"`
+	CreatedAt               time.Time      `json:"created_at"`
+	UpdatedAt               time.Time      `json:"updated_at"`
 }
--- models/userModel.go
+++ models/userModel.go
@@ -2,17 +2,25 @@ package models
 
 import (
 	"time"
-
-	"gorm.io/gorm"
 )
 
+// type User struct {
+// 	gorm.Model
+// 	ID        int
+// 	Name      string
+// 	Mobile    string
+// 	Latitude  float64
+// 	Longitude float64
+// 	CreatedAt time.Time
+// 	UpdatedAt time.Time
+// }
+
 type User struct {
-	gorm.Model
-	ID        int
-	Name      string
-	Mobile    string
-	Latitude  float64
-	Longitude float64
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	ID        int       `gorm:"primary_key" json:"id"`
+	Name      string    `json:"name"`
+	Mobile    string    `json:"mobile"`
+	Latitude  float64   `json:"latitude"`
+	Longitude float64   `json:"longitude"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
