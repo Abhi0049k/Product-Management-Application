f607902786ef1eb69a58ca1c7e480d1623930ce9
Mangalam Kumar <mangalamkumar2002@gmail.com>
2023-11-24 13:51:45 +0530

    Refactoring Codebase

--- controllers/productControllers.go
+++ controllers/productControllers.go
@@ -1,21 +1,17 @@
 package controllers
 
 import (
+	"Product-Management-Application/helpers"
 	"Product-Management-Application/initializers"
 	"Product-Management-Application/models"
 	"fmt"
-	"image"
-	"image/jpeg"
-	"io"
 	"log"
-	"net/http"
 	"os"
 	"path/filepath"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/lib/pq"
-	"github.com/nfnt/resize"
 )
 
 func GetProducts(c *gin.Context) {
@@ -53,21 +49,21 @@ func CreateProduct(c *gin.Context) {
 		destination := filepath.Join("./images", fileName)
 		destination = filepath.ToSlash(destination)
 		localFilePaths = append(localFilePaths, destination)
-		err := downloadImage(imageURL, destination)
+		err := helpers.DownloadImage(imageURL, destination)
 		if err != nil {
 			log.Fatal("Error:", err.Error())
 		}
 
 		compressedFileName := fmt.Sprintf("compressed_%d%s", time.Now().UnixNano(), filepath.Ext(imageURL))
 		compressedDestination := filepath.Join("./compressed-images", compressedFileName)
-		compressedDestination = filepath.ToSlash(destination)
+		compressedDestination = filepath.ToSlash(compressedDestination)
 		err = os.MkdirAll(filepath.Dir(compressedDestination), os.ModePerm)
 
 		if err != nil {
 			log.Fatal("Error:", err.Error())
 		}
 
-		err = compressImage(destination, compressedDestination, 100, 100)
+		err = helpers.CompressImage(destination, compressedDestination, 100, 100)
 		if err != nil {
 			log.Fatal("Error:", err.Error())
 		}
@@ -77,74 +73,14 @@ func CreateProduct(c *gin.Context) {
 
 	product := models.Product{ProductName: body.ProductName, ProductDescription: body.ProductDescription, ProductImages: localFilePaths, CreatedAt: body.CreatedAt, UpdatedAt: body.UpdatedAt, CompressedProductImages: compressedFilePaths}
 
-	result := initializers.DB.Create(&product)
+	// result := initializers.DB.Create(&product)
 
-	if result.Error != nil {
-		c.Status(400)
-		return
-	}
+	// if result.Error != nil {
+	// 	c.Status(400)
+	// 	return
+	// }
 	c.JSON(200, gin.H{
 		"msg":     "Product Created",
 		"product": product,
 	})
-}
-
-func compressImage(source, destination string, maxWidth, maxHeight uint) error {
-	file, err := os.Open(source)
-
-	if err != nil {
-		return err
-	}
-
-	defer file.Close()
-
-	img, _, err := image.Decode(file)
-	if err != nil {
-		return err
-	}
-
-	resizedImg := resize.Resize(maxWidth, maxHeight, img, resize.Lanczos3)
-
-	out, err := os.Create(destination)
-	if err != nil {
-		return err
-	}
-	defer out.Close()
-
-	err = jpeg.Encode(out, resizedImg, nil)
-	if err != nil {
-		return err
-	}
-
-	fmt.Printf("Image compressed successfully to: %s \n", destination)
-	return nil
-
-}
-
-func downloadImage(url, destination string) error {
-	response, err := http.Get(url)
-	if err != nil {
-		return err
-	}
-	defer response.Body.Close()
-
-	err = os.MkdirAll(filepath.Dir(destination), os.ModePerm)
-
-	if err != nil {
-		return err
-	}
-
-	file, err := os.Create(destination)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	_, err = io.Copy(file, response.Body)
-
-	if err != nil {
-		return err
-	}
-	fmt.Printf("Image downloaded successfully")
-	return nil
 }
---
+++ helpers/downloadImage.go
@@ -0,0 +1,35 @@
+package helpers
+
+import (
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+func DownloadImage(url, destination string) error {
+	response, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	err = os.MkdirAll(filepath.Dir(destination), os.ModePerm)
+
+	if err != nil {
+		return err
+	}
+
+	file, err := os.Create(destination)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = io.Copy(file, response.Body)
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
